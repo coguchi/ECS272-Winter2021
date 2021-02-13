537d1a7fc5ee5eecfe9ad43e1b66642ca3a0ce97
coguchi <coguchi@ucdavis.edu>
2021-02-13 07:54:15 -0800

    Get back to this point if things go wrong.

--- Homework3/Templates/es6-webpack-template/node/src/js/barchart.js
+++ Homework3/Templates/es6-webpack-template/node/src/js/barchart.js
@@ -41,6 +41,8 @@ export async function drawBarFromCsvAsync(){
     //draw chart ()
     //drawBarChartCSV(processedData, 'example1');
     drawPCP(data_year, "#PCP");
+
+    //initChartSpace(data_year,"#LineDropdown")
     drawLineDropdown(data_year, "#LineDropdown");
 
 
@@ -63,7 +65,7 @@ function initChartSpace(id,data){
   const svgID = id.substring(1) + "-svg";
 
   const svg = d3.select(id).append("svg")
-    .attr("vieewBox",[0,0,width,height])
+    .attr("viewBox",[0,0,width,height])
     .attr("id",svgID)
     .attr("width","100%")
     .attr("height",height);
@@ -419,126 +421,115 @@ function drawLineDropdown(data, id){
           .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
 
-  // set the dimensions and margins of the graph
-//var margin = {top: 30, right: 80, bottom: 50, left: 45},
-//    width = 280 - margin.left - margin.right,
-//    height = 250 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-//var svg = d3.select(id)
-//  .append("svg")
-//    .attr("width", width + margin.left + margin.right)
-//    .attr("height", height + margin.top + margin.bottom)
-//  .append("g")
-//    .attr("transform",
-//          "translate(" + margin.left + "," + margin.top + ")");
-
-// List of groups (here I have one group per column)
-var allGroup = ["acousticness","danceability","liveness","tempo"];
-
-// add the options to the button
-d3.select("#selectButton")
-  .selectAll('myOptions')
- 	.data(allGroup)
-  .enter()
-	.append('option')
-  .text(function (d) { return d; }) // text showed in the menu
-  .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
 
-// A color scale: one color for each group
-var myColor = d3.scaleOrdinal()
-  .domain(allGroup)
-  .range(d3.schemeSet2);
+
+  // List of groups (here I have one group per column)
+  var allGroup = ["acousticness","danceability","liveness","tempo"];
+
+  // add the options to the button
+  d3.select("#selectButton")
+    .selectAll('myOptions')
+   	.data(allGroup)
+    .enter()
+  	.append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
 
-// Add X axis --> it is a date format
-var x = d3.scaleLinear()
-  .domain([1920,2021])
-  .range([ 0, 400]);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
-
-// text label for the x axis
-svg.append("text")
-  .attr("transform",
-        "translate(" + (width/2) + " ," +
-                       (height + margin.top + 40) + ")")
-  .style("font-size", "12px")
-  .style("text-anchor", "left")
-  .text("Year");
-
-// Add Y axis
-var y = d3.scaleLinear()
-  .domain( [0,1])
-  .range([ height, 0 ]);
-var yAxis = svg.append("g")
-  .call(d3.axisLeft(y));
-
-// text label for the y axis
-svg.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 0 - margin.left)
-  .attr("x",0 - (height / 2))
-  .attr("dy", "1em")
-  .style("font-size", "12px")
-  .style("text-anchor", "middle")
-  .text("Value");
-
-//add title
-svg.append("text")
-  .attr("x", 0)
-  .attr("y", 0 - (margin.top / 2))
-  .attr("text-anchor", "left")
-  .style("font-size", "15px")
-  .style("text-decoration", "underline")
-  .text("The characteristics of music over the years");
-
-// Initialize line with group a
-var line = svg
-  .append('g')
-  .append("path")
-    .datum(data)
-    .attr("d", d3.line()
-      .x(function(d) { return x(+d.year) })
-      .y(function(d) { return y(+d.acousticness) })
-    )
-    //.attr("stroke", function(d){ return myColor("acousticness") })
-    .attr("stroke","violet" )
-    .style("stroke-width", 4)
-    .style("fill", "none")
-
-// A function that update the chart
-function update(selectedGroup,data) {
-
-  // Create new data with the selection?
-  var dataFilter = data.map(function(d){return {year: d.year, value:d[selectedGroup]} })
-
-  console.log(selectedGroup);
-
-  //change the axis according to the value.
-  if (selectedGroup == "tempo") {
-    y.domain([90,130])
-    yAxis.transition().duration(1000).call(d3.axisLeft(y))
-
-  }else{
-    y.domain([0,1])
-    yAxis.transition().duration(1000).call(d3.axisLeft(y))
-  }
+  // A color scale: one color for each group
+  var myColor = d3.scaleOrdinal()
+    .domain(allGroup)
+    .range(d3.schemeSet2);
 
 
-  // Give these new data to update line
-  line
-      .datum(dataFilter)
-      .transition()
-      .duration(1000)
+  // Add X axis --> it is a date format
+  var x = d3.scaleLinear()
+    .domain([1920,2021])
+    .range([ 0, 400]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // text label for the x axis
+  svg.append("text")
+    .attr("transform",
+          "translate(" + (width/2) + " ," +
+                         (height + margin.top + 40) + ")")
+    .style("font-size", "12px")
+    .style("text-anchor", "left")
+    .text("Year");
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain( [0,1])
+    .range([ height, 0 ]);
+  var yAxis = svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // text label for the y axis
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("font-size", "12px")
+    .style("text-anchor", "middle")
+    .text("Value");
+
+  //add title
+  svg.append("text")
+    .attr("x", 0)
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "left")
+    .style("font-size", "15px")
+    .style("text-decoration", "underline")
+    .text("The characteristics of music over the years");
+
+  // Initialize line with group a
+  var line = svg
+    .append('g')
+    .append("path")
+      .datum(data)
       .attr("d", d3.line()
         .x(function(d) { return x(+d.year) })
-        .y(function(d) { return y(+d.value) })
+        .y(function(d) { return y(+d.acousticness) })
       )
-      //.attr("stroke", function(d){ return myColor(selectedGroup) })
-      .attr("stroke", "violet")
+      //.attr("stroke", function(d){ return myColor("acousticness") })
+      .attr("stroke","violet" )
+      .style("stroke-width", 4)
+      .style("fill", "none")
+
+  // A function that update the chart
+  function update(selectedGroup,data) {
+
+    // Create new data with the selection?
+    var dataFilter = data.map(function(d){return {year: d.year, value:d[selectedGroup]} })
+
+    console.log(selectedGroup);
+
+    //change the axis according to the value.
+    if (selectedGroup == "tempo") {
+      y.domain([90,130])
+      yAxis.transition().duration(1000).call(d3.axisLeft(y))
+
+    }else{
+      y.domain([0,1])
+      yAxis.transition().duration(1000).call(d3.axisLeft(y))
+    }
+
+
+    // Give these new data to update line
+    line
+        .datum(dataFilter)
+        .transition()
+        .duration(1000)
+        .attr("d", d3.line()
+          .x(function(d) { return x(+d.year) })
+          .y(function(d) { return y(+d.value) })
+        )
+        //.attr("stroke", function(d){ return myColor(selectedGroup) })
+        .attr("stroke", "violet")
 
 
 
